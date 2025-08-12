873aea315708fb5b95d6993ed1768e9000a883e2
shivam mishra <sm20160168284@outlook.com>
2025-08-12 20:38:40 +0530

    vectors

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -1,12 +1,20 @@
 #include<iostream>
+#include <vector>
 using namespace std;
 
 int main(){
+        vector<int> v (4,1); //* here 4 is the size of vector and 2 is the value of v vector it also give 2 to every index
+       //* 2 2 2 2
+       
+        v.push_back(5);
+        v.push_back(6);
+        v.push_back(5);
 
+        for(int i=0;i<v.size();i++){
+            cout<<v[i]<<" ";
+        }
 
-    
-
-
+        //* 2 2 2 2 5 6 5
 
     return 0;
 }
