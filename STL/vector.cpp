780eedba71da71ea2ad6bcb0d222758c512c6dcc
shivam mishra <sm20160168284@outlook.com>
2025-08-14 12:23:50 +0530

    vectors

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -12,9 +12,33 @@ int main(){
 
         for(int i=0;i<v.size();i++){
             cout<<v[i]<<" ";
-        }
+        }  
 
         //* 2 2 2 2 5 6 5
+
+         //* vector is also a dynamic type allocation memory
+         //* vector is stored in heap memory unlike array which is stored in stack
+
+        //* push_back time complexity - O(1)
+
+        //* Remove value from vector
+
+        cout<<endl;
+
+        v.pop_back();
+        for(int i=0;i<v.size();i++){
+            cout<<v[i]<<" ";
+        }
+
+        //* 2 2 2 2 5 6 
+
+        //** v.clear(); // this will remove all value in vector
+
+        cout<<"\n"<<v.front();
+        cout<<"\n"<<v.back();
+        cout<<"\n"<<v.empty();
+        cout<<"\n"<<v.at(5);
+
 
     return 0;
 }
